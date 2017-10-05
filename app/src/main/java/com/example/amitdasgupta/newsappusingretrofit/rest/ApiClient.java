2027f387231be215b086a5ce23eb4332b77dad51
Amit Das Gupta <amitdsgupta007@gmail.com>
2017-10-05 21:03:52 +0530

    News Layout

--- app/src/main/java/com/example/amitdasgupta/newsappusingretrofit/rest/ApiClient.java
+++ app/src/main/java/com/example/amitdasgupta/newsappusingretrofit/rest/ApiClient.java
@@ -3,7 +3,6 @@ package com.example.amitdasgupta.newsappusingretrofit.rest;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-
 public class ApiClient {
     public static final String BASE_URL = "https://newsapi.org/v1/";
     private static Retrofit retrofit = null;
